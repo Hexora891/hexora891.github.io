7bf0dfa0add1dc08245ff842e7bc765f6d005ca4
Sasta-Hacker <gamingarmyyt123@gmail.com>
2025-08-23 17:42:50 +0000

    Retro desktop with icons and window controls

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,7 +17,45 @@ function closeWindow(id) {
     win.style.display = "none";
 }
 
+// Minimize/Maximize
+function minimizeWindow(id) {
+    const win = document.getElementById(id);
+    win.querySelector('.window-content').style.display = 'none';
+    win.style.height = '30px';
+}
+
+function maximizeWindow(id) {
+    const win = document.getElementById(id);
+    const content = win.querySelector('.window-content');
+    if (win.style.height === '30px') {
+        content.style.display = 'block';
+        win.style.height = '300px';
+    } else {
+        content.style.display = 'none';
+        win.style.height = '30px';
+    }
+}
+
 // Fake resume download
 function downloadResume() {
     alert("Downloading résumé... (add real file later!)");
 }
+
+// Draggable windows
+let dragged;
+document.querySelectorAll('.window-header').forEach(header => {
+    header.onmousedown = function(e) {
+        dragged = this.parentElement;
+        let offsetX = e.clientX - dragged.offsetLeft;
+        let offsetY = e.clientY - dragged.offsetTop;
+        function moveHandler(e) {
+            dragged.style.left = (e.clientX - offsetX) + 'px';
+            dragged.style.top = (e.clientY - offsetY) + 'px';
+        }
+        document.addEventListener('mousemove', moveHandler);
+        document.onmouseup = () => {
+            document.removeEventListener('mousemove', moveHandler);
+            document.onmouseup = null;
+        }
+    }
+});
