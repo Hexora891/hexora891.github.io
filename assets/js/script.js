5a1b15b66beecb9db0415cf2cccc08ebbb2b9e2e
Sasta-Hacker <gamingarmyyt123@gmail.com>
2025-08-29 00:14:57 +0530

    Test SSH setup

--- assets/js/script.js
+++ assets/js/script.js
@@ -174,11 +174,11 @@ function removeTaskbarButton(id) {
 // Resume function
 function downloadResume() {
   const link = document.createElement('a');
-  link.href = "https://drive.google.com/file/d/1qKZmJfNHu_RzCEbR8kFtqQeBEAW6iJC_/view?usp=drive_link";
+  link.href = "https://drive.google.com/file/d/1qKZmJfNHu_RzCEbR8kFtqQeBEAW6iJC_/preview";
   link.download = "Ayush-Resume.pdf";
   link.click();
 }
 
 function openResumeNewTab() {
-  window.open("https://drive.google.com/file/d/1qKZmJfNHu_RzCEbR8kFtqQeBEAW6iJC_/view?usp=drive_link", "_blank");
+  window.open("https://drive.google.com/file/d/1qKZmJfNHu_RzCEbR8kFtqQeBEAW6iJC_/preview", "_blank");
 }
